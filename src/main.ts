07545a973f7b38529c90f74ca05a672d074969ba
yoshimaru46 <yshr446@gmail.com>
2022-01-08 17:47:43 +0900

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'https://next-realworld-example-app-omega.vercel.app/',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
